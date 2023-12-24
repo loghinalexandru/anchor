af95acaae2242b6dd300de308d6c210f6a751dc9
Loghin Alexandru <loghinalexandru61@gmail.com>
2023-12-24 14:21:51 +0200

    Add more test cases

--- internal/command/util/parser/netscape_test.go
+++ internal/command/util/parser/netscape_test.go
@@ -1,7 +1,6 @@
 package parser_test
 
 import (
-	"errors"
 	"fmt"
 	"io/fs"
 	"os"
@@ -25,24 +24,38 @@ func TestTraversalComplex(t *testing.T) {
 	dir := t.TempDir()
 	want := []TraversalTest{
 		{
-			name:    "gan",
-			content: "asd",
+			name: "gan",
+			content: `"Introduction to GANs with Python and TensorFlow" "https://stackabuse.com/introduction-to-gans-with-python-and-tensorflow/"
+"sklearn.datasets.fetch_lfw_people — scikit-learn 0.24.1 documentation" "https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_lfw_people.html"
+"A Beginner's Guide to Generative Adversarial Networks (GANs) | Pathmind" "https://wiki.pathmind.com/generative-adversarial-network-gan"
+"mnist-gan/gan.py at master · gtoubassi/mnist-gan" "https://github.com/gtoubassi/mnist-gan/blob/master/gan.py"
+"GitHub - soumith/ganhacks: starter from \"How to Train a GAN?\" at NIPS2016" "https://github.com/soumith/ganhacks"
+`,
+		},
+		{
+			name: "gan.research-papers",
+			content: `"GAN - 2014 paper" "https://arxiv.org/pdf/1406.2661.pdf"
+"LSGAN.pdf" "https://arxiv.org/pdf/1611.04076.pdf"
+"Internal Covariate Shift.pdf" "https://arxiv.org/pdf/1502.03167.pdf"
+"https://arxiv.org/pdf/1903.06048.pdf" "https://arxiv.org/pdf/1903.06048.pdf"
+"https://arxiv.org/pdf/1802.05957.pdf" "https://arxiv.org/pdf/1802.05957.pdf"
+`,
 		},
 	}
 
 	input, err := os.ReadFile("testdata/complex.input")
 	if err != nil {
-		t.Fatalf("unexpected error when reading input file; got %q", err)
+		t.Fatalf("unexpected error when reading input file; got %s", err)
 	}
 
 	doc, err := netscape.Unmarshal(input)
 	if err != nil {
-		t.Fatalf("unexpected error when parsing input file; got %q", err)
+		t.Fatalf("unexpected error when parsing input file; got %s", err)
 	}
 
 	err = parser.Traversal(dir, nil, doc.Root)
 	if err != nil {
-		t.Fatalf("unexpected error; got %q", err)
+		t.Fatalf("unexpected error; got %s", err)
 	}
 
 	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
@@ -59,7 +72,8 @@ func TestTraversalComplex(t *testing.T) {
 		})
 
 		if testCase == -1 {
-			return errors.New(fmt.Sprintf("could not find file name %q", d.Name()))
+			t.Errorf(fmt.Sprintf("could not find file name %q", d.Name()))
+			return nil
 		}
 
 		got, err := os.ReadFile(path)
@@ -68,12 +82,12 @@ func TestTraversalComplex(t *testing.T) {
 		}
 
 		if want[testCase].content != string(got) {
-			return errors.New(fmt.Sprintf("unexpected content for file %q; (-want +got):\\n%s", d.Name(), cmp.Diff(want[testCase].content, string(got))))
+			t.Errorf("mismatch content; (-want +got):\n %s", cmp.Diff(want[testCase].content, string(got)))
 		}
 
 		return nil
 	})
 	if err != nil {
-		t.Fatalf("unexpected error; got %q", err)
+		t.Fatalf("unexpected error; got %s", err)
 	}
 }
